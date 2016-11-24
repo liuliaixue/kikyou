d827690529c513ed66be62a78d3e653873e6f72f
alan <614435388@qq.com>
2016-11-24 23:42:28 +0800

    u

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var fs = require('fs');
 var tools = require('./modules/tools');
 
 app.use(cookieParser('alan'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
     secret: '12345',
@@ -74,7 +74,69 @@ app.use('/user', user); // mini app，通常做应用拆分
 
 
 
+// 聊天室 start
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// app.get("/chat", function (req, res, next) {
+//     res.send("welcome to chatroom");
+// });
+
+
+//在线用户
+var onlineUsers = {};
+//当前在线人数
+var onlineCount = 0;
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    //监听新用户加入
+    socket.on('login', function (obj) {
+        //将新加入用户的唯一标识当作socket的名称，后面退出的时候会用到
+        socket.name = obj.userid;
+
+        //检查在线列表，如果不在里面就加入
+        if (!onlineUsers.hasOwnProperty(obj.userid)) {
+            onlineUsers[obj.userid] = obj.username;
+            //在线人数+1
+            onlineCount++;
+        }
+
+        //向所有客户端广播用户加入
+        io.emit('login', { onlineUsers: onlineUsers, onlineCount: onlineCount, user: obj });
+        console.log(obj.username + '加入了聊天室');
+    });
+
+    //监听用户退出
+    socket.on('disconnect', function () {
+        //将退出的用户从在线列表中删除
+        if (onlineUsers.hasOwnProperty(socket.name)) {
+            //退出用户的信息
+            var obj = { userid: socket.name, username: onlineUsers[socket.name] };
+
+            //删除
+            delete onlineUsers[socket.name];
+            //在线人数-1
+            onlineCount--;
+
+            //向所有客户端广播用户退出
+            io.emit('logout', { onlineUsers: onlineUsers, onlineCount: onlineCount, user: obj });
+            console.log(obj.username + '退出了聊天室');
+        }
+    });
+
+    //监听用户发布聊天内容
+    socket.on('message', function (obj) {
+        //向所有客户端广播发布的消息
+        io.emit('message', obj);
+        console.log(obj.username + '说：' + obj.content);
+    });
+
+});
+
+// 聊天室 end
 
 var server = app.listen(8081, function () {
 
