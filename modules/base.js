a83d9002682e2ebb6ecaffb8c4f7b85e43b5a71d
alan <614435388@qq.com>
2017-02-27 18:49:03 +0800

    update s

---
+++ modules/base.js
@@ -0,0 +1,23 @@
+// base.js
+
+var BaseModule = function(){
+
+};
+
+BaseModule.prototype.addItem = function() {
+	return "not support it yet";
+};
+
+BaseModule.prototype.getList = function() {
+	return "not support it yet";
+};
+
+BaseModule.prototype.getDetail = function() {
+	return "not support it yet";
+};
+
+BaseModule.prototype.deleteItem = function() {
+	return "not support it yet";
+};
+
+module.exports = BaseModule;
